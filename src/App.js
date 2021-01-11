6be1ff48a4376dc4bd37a2a32fb0c71c3f71973d
josephmathew900 <josephmathew900@gmail.com>
2021-01-11 22:03:13 +0530

    component + style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import './App.css';
+import Todos from './components/Todos'
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id : 1,
+        title: 'Take out the trash',
+        completed: true
+      },
+      {
+        id : 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id : 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
   render() {
+    console.log(this.state.todos)
     return (
-    <div className="App">
-      <h1>App</h1>
+    <div>
+      <Todos todos={this.state.todos} />
     </div>
   );}
 }
